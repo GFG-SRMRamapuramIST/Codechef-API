c2997579024225df62eda37ccb5ef794cf33dcaf
VISHAL KUAMR YADAV <vishal100403@gmail.com>
2025-02-17 11:28:02 +0530

    Update API routes in scraperRoutes.js to include '/api' prefix

--- routers/scraperRoutes.js
+++ routers/scraperRoutes.js
@@ -14,22 +14,22 @@ const router = new express.Router();
 router.get("/", (req, res) => {
   const apiDetails = [
     {
-      route: "/info/:username",
+      route: "/api/info/:username",
       method: "GET",
       description: "Fetches user information from CodeChef by username.",
     },
     {
-      route: "/heat-map/:username",
+      route: "/api/heat-map/:username",
       method: "GET",
       description: "Fetches the heat map data from CodeChef by username.",
     },
     {
-      route: "/rating-graph/:username",
+      route: "/api/rating-graph/:username",
       method: "GET",
       description: "Fetches the rating graph data from CodeChef by username.",
     },
     {
-      route: "/contest-data/:username",
+      route: "/api/contest-data/:username",
       method: "GET",
       description: "Fetches the contest data from CodeChef by username.",
     },
